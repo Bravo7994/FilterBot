007b0df94c1c5dc0dac825a679a46acac01405b1
Johny Bravo <90997181+Bravo7994@users.noreply.github.com>
2022-11-17 18:59:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import logging
 import logging.config
+from aiohttp import web
+from plugins import web_server
 
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
@@ -11,8 +13,10 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, PORT
 from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
 
 class Bot(Client):
 
